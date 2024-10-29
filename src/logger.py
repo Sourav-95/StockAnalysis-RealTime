37f53c9714198eaf403688c9c11618a92c154e76
sourav <souravmaity16@gmail.com>
2024-10-30 01:51:38 +0530

    Modified Logger File

--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,32 @@
+# logger.py
 import logging
 import os
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
+# Generate dynamic log file name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Create a default logger instance
+logger = logging.getLogger('app_logger')
+logger.setLevel(logging.INFO)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-# if __name__ =="__main__":
-#     print("Logging has started on Mac")
+# File handler to save logs to a dynamically named log file
+file_handler = RotatingFileHandler(LOG_FILE_PATH, maxBytes=5*1024*1024, backupCount=5)
+file_handler.setLevel(logging.INFO)
+
+# Console handler to display logs on the console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Set log formatting
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
